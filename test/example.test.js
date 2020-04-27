81e4bbb0bc70daf1702592f6263a4f18a30e0165
Naimur Rahman Sourov <naimur.rahman103@gmail.com>
2020-04-27 09:33:56 +0600

    init test

---
+++ test/example.test.js
@@ -0,0 +1,22 @@
+const puppeteer = require('puppeteer');
+const { expect } = require('chai');
+
+let browser;
+let page;
+
+before(async () => {
+	browser = await puppeteer.launch({ headless: false });
+	page = await browser.newPage();
+});
+
+describe('Check Example Homepage', () => {
+	it('has title "Example Domain"', async () => {
+		await page.goto('https://example.com', { waitUntil: 'networkidle0' });
+		const title = await page.title();
+		expect(title).equal('Example Domain');
+	}).timeout(10000);
+});
+
+after(async () => {
+	await browser.close();
+});
