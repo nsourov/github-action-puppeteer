a5b5581fa0ea08aa3d32c0d46df46e50bd1bfa8d
Naimur Rahman Sourov <naimur.rahman103@gmail.com>
2020-04-27 09:51:19 +0600

    test with puppeteer

--- test/example.test.js
+++ test/example.test.js
@@ -4,20 +4,20 @@ const { expect } = require('chai');
 let browser;
 let page;
 
-// before(async () => {
-// 	browser = await puppeteer.launch({ headless: true });
-// 	page = await browser.newPage();
-// });
+before(async () => {
+	browser = await puppeteer.launch({ headless: true });
+	page = await browser.newPage();
+});
 
 describe('Check Example Homepage', () => {
 	it('has title "Example Domain"', async () => {
-		// await page.goto('https://example.com', { waitUntil: 'networkidle0' });
-		// const title = await page.title();
-		// expect(title).equal('Example Domain');
+		await page.goto('https://example.com', { waitUntil: 'networkidle0' });
+		const title = await page.title();
+		expect(title).equal('Example Domain');
 		expect('Example Domain').equal('Example Domain');
 	}).timeout(10000);
 });
 
-// after(async () => {
-// 	await browser.close();
-// });
+after(async () => {
+	await browser.close();
+});
