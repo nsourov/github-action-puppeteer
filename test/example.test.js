1c2a45a7483c7d3bea0caedff4b1ab9f97a338c2
Naimur Rahman Sourov <naimur.rahman103@gmail.com>
2020-04-27 09:57:26 +0600

    try in headfull

--- test/example.test.js
+++ test/example.test.js
@@ -5,7 +5,7 @@ let browser;
 let page;
 
 before(async () => {
-	browser = await puppeteer.launch({ headless: true });
+	browser = await puppeteer.launch({ headless: false });
 	page = await browser.newPage();
 });
 
